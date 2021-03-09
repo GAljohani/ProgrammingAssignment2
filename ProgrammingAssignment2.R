cb833aa11dabbb61564690063845abde87c079ce
Ghazi Aljohani <ghazialmarwani@gmail.com>
2021-03-09 16:37:37 +0000

    Hopefully this works

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,25 @@
+makeChacheMatrix <- function(x = matrix()) {
+  Inv <- NULL
+  set <- function(y) {
+    x <<- y
+    Inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) Inv <<- inverse
+  getInverse <- function() Inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...) {
+  Inv <- x$getInverse()
+  if(!is.null(Inv)) {
+    message("getting cached data")
+    return(Inv)
+  }
+  data <- x$get()
+  Inv <- Solve(data, ...)
+  x$setInverse(Inv)
+  Inv
+}
